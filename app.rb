efc7ba7fe2cbe806f203e5d8c46ce1fa6432e341
Natalie <natsetc@gmail.com>
2019-07-01 22:02:26 -0700

    add notes

--- app.rb
+++ app.rb
@@ -5,17 +5,27 @@ class App < Sinatra::Base
   get '/reversename/:name' do
     @name = params[:name]
     @name.reverse
+
+    # params[:name].reverse
   end
 
   get '/square/:number' do 
     @sqnum = params[:number].to_i ** 2
     "#{@sqnum}"
+
+    # (params[:number].to_i ** 2).to_s
   end
 
   get '/say/:number/:phrase' do
     @phrase = params[:phrase] 
     @number = params[:number].to_i
     (@phrase + " ") * @number
+
+    # answer = ''
+    # params[:number].to_i.times do
+    #   answer += params[:phrase]
+    # end
+    # answer
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
@@ -36,6 +46,21 @@ class App < Sinatra::Base
     when "divide"
       "#{@number1/@number2}"
     end
+
+    # number1 = params[:number1].to_i
+    # number2 = params[:number2].to_i
+    # answer = 'Unable to perform this operation'
+    # case params[:operation]
+    # when 'add'
+    #   answer = (number1 + number2).to_s
+    # when 'subtract'
+    #   answer = (number1 - number2).to_s
+    # when 'multiply'
+    #   answer = (number1 * number2).to_s
+    # when 'divide'
+    #   answer = (number1 / number2).to_s
+    # end
+
   end
 
 end
